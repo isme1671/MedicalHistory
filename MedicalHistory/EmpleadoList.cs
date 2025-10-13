b8ac7fa219de8c09105b7c2fc133483d42984f45
isme1671 <ismelin74@gmail.com>
2025-10-13 14:36:13 -0600

    Squashed commit of the following:
    commit 2f865108c1314f3665a0d88014ddd8b2751ddb15
    Merge: a841de2 632e545
    Author: isme1671 <ismelin74@gmail.com>
    Date:   Mon Oct 13 14:18:54 2025 -0600
    
        Merge branch 'master' into pr/1
    
    commit a841de237e27abadbfc3fac45fdec094fd583ffc
    Author: Angel Manuel Francis Santana <angelfrancissantana2002@gmail.com>
    Date:   Mon Oct 13 16:29:10 2025 +0100
    
        Registro empleados

---
+++ MedicalHistory/EmpleadoList.cs
@@ -0,0 +1,31 @@
+﻿using MedicalHistory.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalHistory
+{
+    public partial class EmpleadoList : Form
+    {
+        public EmpleadoList()
+        {
+            InitializeComponent();
+
+            EmpleadosSql em = new EmpleadosSql();
+
+            dgem.DataSource = em.Mostrar();
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            Empleados em = new Empleados();
+            em.Show();
+        }
+    }
+}
